cce502e44deccc104ac4a668e29ce5cdbe5cdbb5
Matteo Magnani <matteo.magnani18@studio.unibo.it>
2018-08-27 13:00:52 +0200

    add missing properties to PlantData trait

--- src/main/scala/it/unibo/pps/ese/controller/loader/data/PlantData.scala
+++ src/main/scala/it/unibo/pps/ese/controller/loader/data/PlantData.scala
@@ -3,5 +3,7 @@ package it.unibo.pps.ese.controller.loader.data
 trait PlantData extends EntityData {
   def height: Double
   def nutritionalValue: Double
+  def attractiveness: Double
+  def hardness: Double
   def availability: Double
 }
