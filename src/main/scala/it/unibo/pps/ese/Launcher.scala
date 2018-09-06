2536f4d50d70f50b88f3dc43c6c3ade75b42f079
Matteo Magnani <matteo.magnani18@studio.unibo.it>
2018-09-06 20:25:52 +0200

    update main launcher

--- src/main/scala/it/unibo/pps/ese/Launcher.scala
+++ src/main/scala/it/unibo/pps/ese/Launcher.scala
@@ -1,24 +1,30 @@
 package it.unibo.pps.ese
 
+import it.unibo.pps.ese.genericworld.TestLauncher.stage
 import it.unibo.pps.ese.genericworld.controller.Controller
 import it.unibo.pps.ese.genericworld.model.SimulationBuilder.Simulation.EmptySimulation
 import it.unibo.pps.ese.genericworld.model.{SimulationBuilder, World}
 import it.unibo.pps.ese.genetics.GeneticsSimulator
 import it.unibo.pps.ese.view.View
 import scalafx.application.JFXApp
+import scalafx.stage.WindowEvent
 
-import scala.concurrent.duration._
+import scalafx.Includes._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Launcher extends JFXApp {
   require(parameters.raw.size == 1, "Application requires an input path corresponding to a simulation's config file")
-  //val world: World = WorldBuilder buildWorldFromSimulationData (parameters.raw.head, 500, 500)
-  //val controller = Controller(world, clockPeriod = 250 millis)
+  
   val controller: Controller =
-  new SimulationBuilder[EmptySimulation] dimension(500, 500) data "it/unibo/pps/ese/controller/loader/Simulation.yml" build
+  new SimulationBuilder[EmptySimulation] dimension(500, 500) data parameters.raw.head build
+
   val view = View(GeneticsSimulator)
   stage = view
   controller attachView (view, frameRate = 30)
   controller.manage.play()
+
+  stage.setOnCloseRequest((e: WindowEvent) => {
+    controller.manage.exit()
+  })
 
 }
