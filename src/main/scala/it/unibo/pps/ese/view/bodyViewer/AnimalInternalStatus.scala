7c1e37755c9915e86676c624197cca50f40da460
Diego Pergolini <diego.pergolini@studio.unibo.it>
2018-09-17 18:05:50 +0200

    Added some Scaladoc

--- src/main/scala/it/unibo/pps/ese/view/bodyViewer/AnimalInternalStatus.scala
+++ src/main/scala/it/unibo/pps/ese/view/bodyViewer/AnimalInternalStatus.scala
@@ -23,18 +23,38 @@ sealed trait DigestiveSystemStatus
 case object Digesting extends DigestiveSystemStatus
 case object NotDigesting extends DigestiveSystemStatus
 
+/**
+  * Trait to obtain the internal status of an Animal
+  */
 sealed trait  AnimalInternalStatus {
   def brainStatus:BrainStatus
   def eyesStatus:EyesStatus
   def reproductiveApparatusStatus:ReproductiveApparatusStatus
   def digestiveSystemStatus:DigestiveSystemStatus
 }
+
+/**
+  * Case class describing the internal status of a Male Animal
+  * @param brainStatus
+  * @param eyesStatus
+  * @param reproductiveApparatusStatus
+  * @param digestiveSystemStatus
+  */
 case class MaleInternalStatus(
                                brainStatus: BrainStatus,
                                eyesStatus: EyesStatus,
                                reproductiveApparatusStatus: ReproductiveApparatusStatus,
                                digestiveSystemStatus:DigestiveSystemStatus
                              )extends AnimalInternalStatus
+
+/**
+  * Case class describing the internal status of a Female Animal
+  * @param brainStatus
+  * @param eyesStatus
+  * @param reproductiveApparatusStatus
+  * @param digestiveSystemStatus
+  * @param fetusStatus
+  */
 case class FemaleInternalStatus(
                                  brainStatus: BrainStatus,
                                  eyesStatus: EyesStatus,
