f49c411bcd1f476512649896d5019a6190eb1920
Matteo Magnani <matteo.magnani18@studio.unibo.it>
2018-09-27 19:07:03 +0200

    add IO docs

--- src/main/scala/it/unibo/pps/ese/controller/simulation/loader/io/Folder.scala
+++ src/main/scala/it/unibo/pps/ese/controller/simulation/loader/io/Folder.scala
@@ -9,30 +9,67 @@ import org.apache.commons.io.FilenameUtils
 import it.unibo.pps.ese.controller.simulation.loader.io.File.FileFormat
 import org.apache.commons.io.FileUtils
 
+/** Trait that defines a folder that already exists*/
 trait Folder extends ExistingResource with FolderResource {
-  def getFiles: Seq[File]
-  def getFiles(fileFormat: FileFormat): Seq[File]
-  def getFilesAsStream: Seq[InputStream]
-  def getFilesAsStream(fileFormat: FileFormat): Seq[InputStream]
+
+  /**
+    * @param fileFormats Optional file formats used as filter
+    * @return Files contained in the folder
+    */
+  def getFiles(fileFormats: FileFormat*): Seq[File]
+
+  /**
+    * @param fileFormats Optional file formats used as filter
+    * @return Files contained in the folder as InputStream
+    */
+  def getFilesAsStream(fileFormats: FileFormat*): Seq[InputStream]
+
+  /** Obtain children resource with relative path starting from this folder as input
+    *
+    * @param relativePath Relative path to required children
+    * @return An option containing children if exists
+    */
   def getExistingChildren(relativePath: String): Option[ExistingResource]
 }
 
+/** Factory object for [[it.unibo.pps.ese.controller.simulation.loader.io.Folder]]*/
 object Folder {
 
+  /** Creates [[it.unibo.pps.ese.controller.simulation.loader.io.Folder]] starting from its path as URL
+    *
+    * @param folderPath Folder's URL
+    * @return Required Folder
+    */
   def apply(folderPath: URL): Folder = new FolderImpl(folderPath)
+
+  /** Creates [[it.unibo.pps.ese.controller.simulation.loader.io.Folder]] starting from its path as String
+    *
+    * @param folderPath Folder's path
+    * @return Required Folder
+    */
   def apply(folderPath: String): Folder = Folder(new io.File(folderPath))
+
+  /** Creates [[it.unibo.pps.ese.controller.simulation.loader.io.Folder]] starting from a java File
+    *
+    * @param file Java file
+    * @return Required Folder
+    */
   def apply(file: java.io.File): Folder = new FolderImpl(file.toURI.toURL)
 
-  private class FolderImpl(folderPath: URL) extends ExistingResourceImpl(folderPath) with Folder {
+  private[this] class FolderImpl(folderPath: URL) extends ExistingResourceImpl(folderPath) with Folder {
     require(javaFile.isDirectory)
 
     def getFiles: Seq[File] = javaFile.listFiles().filter(_.isFile).map(File(_)).toSeq
 
-    def getFiles(fileFormat: FileFormat): Seq[File] = getFiles.filter(f => fileFormat.extensions.exists(ext => f.name.endsWith(ext)))
+    def getFiles(fileFormats: FileFormat*): Seq[File] = {
+      var files = javaFile.listFiles().filter(_.isFile).map(File(_))
+      if(fileFormats.nonEmpty) {
+        files = files.filter(f => fileFormats.flatMap(_.extensions).exists(ext => f.name.endsWith(ext)))
+      }
+      files.toSeq
+    }
 
-    def getFilesAsStream: Seq[InputStream] = convertToInputStream(getFiles)
-
-    def getFilesAsStream(fileFormat: FileFormat): Seq[InputStream] = convertToInputStream(getFiles(fileFormat))
+    def getFilesAsStream(fileFormats: FileFormat*): Seq[InputStream] = convertToInputStream(getFiles(fileFormats:_*))
 
     private def convertToInputStream(urls: Seq[File]): Seq[InputStream] = urls.map(_.rawFile).map(FileUtils.openInputStream)
 
--- src/main/scala/it/unibo/pps/ese/controller/simulation/loader/io/IOResource.scala
+++ src/main/scala/it/unibo/pps/ese/controller/simulation/loader/io/IOResource.scala
@@ -5,16 +5,37 @@ import java.net.URL
 import it.unibo.pps.ese.controller.simulation.loader.io.File.FileFormat
 import org.apache.commons.io.FilenameUtils
 
+/** Trait that defines a generic IO resource*/
 trait IOResource {
+  /**
+    * @return Parent. It is a folder, that can exists or not
+    */
   def getParent: Option[FolderResource]
+
+  /**
+    * @return Resource name
+    */
   def getName: String
 }
 
+/** Trait that defines a generic IO resource that already exists*/
 trait ExistingResource extends IOResource {
+  /**
+    * @return Parent folder
+    */
   def getParentFolder: Option[Folder]
 }
 
+/** Trait that defines a generic IO resource  that not exists*/
+sealed trait NotExistingResource extends IOResource
+
+/** Trait that defines a folder IO resource, that can exist or not*/
 trait FolderResource extends IOResource {
+  /** Method returns existing folder corresponding to this folder resource. If the folder not exists, it is created.
+    * The creation process fails if the resource needs super folders creation
+    *
+    * @return Required folder
+    */
   def getOrCreateFolder(): Option[Folder] = this match {
     case f: NotExistingFolder =>
       f.createFolder()
@@ -22,9 +43,22 @@ trait FolderResource extends IOResource {
       Some(f)
   }
 
+  /** Obtain children resource with relative path starting from this folder as input
+    *
+    * @param relativePath Relative path to required children
+    * @return Children IOResource that can be matched to check children status
+    */
   def getChildren(relativePath: String): IOResource
 }
+
+/** Trait that defines a file IO resource, that can exist or not*/
 trait FileResource extends IOResource {
+
+  /** Method returns existing file corresponding to this folder resource. If the file not exists, it is created.
+    * The creation process fails if the resource needs super folders creation
+    *
+    * @return Required file
+    */
   def getOrCreateFile(): Option[File] = this match {
     case f: NotExistingFile =>
       f.createFile()
@@ -33,15 +67,27 @@ trait FileResource extends IOResource {
   }
 }
 
-sealed trait NotExistingResource extends IOResource
-
+/** Trait that defines a folder that not already exists*/
 sealed trait NotExistingFolder extends NotExistingResource with FolderResource {
+  /** Method creates not existing folder. The process fails if the resource needs super folders
+    * creation
+    *
+    * @return Created folder
+    */
   def createFolder(): Option[Folder]
 }
+
+/** Trait that defines a file that not already exists*/
 sealed trait NotExistingFile extends NotExistingResource with FileResource {
+  /** Method creates not existing file. The process fails if the resource needs super folders
+    * creation
+    *
+    * @return Created file
+    */
   def createFile(): Option[File]
 }
 
+/** Trait that defines an undefined IO resource that not already exists*/
 sealed trait UndefinedNotExistingResource extends NotExistingFolder with NotExistingFile {
   def hasFileExtension(format: FileFormat): Boolean
 }
@@ -82,12 +128,23 @@ sealed abstract class IOResourceImpl(path: URL) extends IOResource {
   override def toString: String = url.toString
 }
 
+/** Factory object to create IO resources starting from various representations*/
 object IOResource {
 
+  /** Creates the IO resource corresponding to given representation. Result can be matched to identify resource status
+    *
+    * @param path Resource's path
+    * @return Resource
+    */
   def apply(path: String): IOResource = {
     apply(new java.io.File(path).toURI.toURL)
   }
 
+  /** Creates the IO resource corresponding to given representation. Result can be matched to identify resource status
+    *
+    * @param path Resource's URL
+    * @return Resource
+    */
   def apply(path: URL): IOResource = {
     val file: java.io.File = new java.io.File(path.toURI)
     if(file.exists()) {
