85a1a8d46ad1d7437d6dd4eae8e0827e8a2bdd0d
Matteo Magnani <matteo.magnani18@studio.unibo.it>
2018-09-28 10:44:43 +0200

    add missing docs

--- src/main/scala/it/unibo/pps/ese/controller/simulation/loader/data/builder/exception/CompleteBuildException.scala
+++ src/main/scala/it/unibo/pps/ese/controller/simulation/loader/data/builder/exception/CompleteBuildException.scala
@@ -1,5 +1,8 @@
 package it.unibo.pps.ese.controller.simulation.loader.data.builder.exception
 
+/** Specialization of [[it.unibo.pps.ese.controller.simulation.loader.data.builder.exception.HierarchyMotivationsException]]
+  * that indicated a problem in a builder's complete build
+  */
 trait CompleteBuildException extends HierarchyMotivationsException {
   override type MixedException = CompleteBuildException
 
@@ -12,14 +15,29 @@ trait CompleteBuildException extends HierarchyMotivationsException {
   }
 }
 
+/** Factory object for [[it.unibo.pps.ese.controller.simulation.loader.data.builder.exception.CompleteBuildException]]*/
 object CompleteBuildException {
 
+  /**
+    * @param motivations Motivations
+    * @return New [[it.unibo.pps.ese.controller.simulation.loader.data.builder.exception.CompleteBuildException]]
+    */
   def apply(motivations: Iterable[Motivation]): CompleteBuildException = new AbsHierarchyMotivationsException(motivations) with CompleteBuildException
 
+  /**
+    * @param motivation Motivation as string
+    * @param subMotivations Sub motivations
+    * @return New [[it.unibo.pps.ese.controller.simulation.loader.data.builder.exception.CompleteBuildException]]
+    */
   def apply(motivation: String, subMotivations: Iterable[Motivation] = Seq()): CompleteBuildException = {
     new AbsHierarchyMotivationsException(motivation, subMotivations) with CompleteBuildException
   }
 
+  /**
+    * @param motivation Motivation as string
+    * @param exc Exceptions that indicates sub motivations
+    * @return New [[it.unibo.pps.ese.controller.simulation.loader.data.builder.exception.CompleteBuildException]]
+    */
   def apply(motivation: String, exc: Iterable[HierarchyMotivationsException])(implicit i: DummyImplicit): CompleteBuildException = {
     new AbsHierarchyMotivationsException(motivation, exc) with CompleteBuildException
   }
