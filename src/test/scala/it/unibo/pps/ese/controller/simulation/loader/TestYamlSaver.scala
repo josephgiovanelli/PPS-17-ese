5e20611e1cee6453531fa307b35516763c94e201
Matteo Magnani <matteo.magnani18@studio.unibo.it>
2018-09-29 15:36:32 +0200

    add missing test

--- src/test/scala/it/unibo/pps/ese/controller/simulation/loader/TestYamlSaver.scala
+++ src/test/scala/it/unibo/pps/ese/controller/simulation/loader/TestYamlSaver.scala
@@ -4,7 +4,8 @@ package it.unibo.pps.ese.controller.simulation.loader
 import java.nio.file.Files
 
 import it.unibo.pps.ese.controller.simulation.loader.data.SimulationData.CompleteSimulationData
-import it.unibo.pps.ese.controller.simulation.loader.io.{File, Folder}
+import it.unibo.pps.ese.controller.simulation.loader.exception.ResourceAlreadyExistsException
+import it.unibo.pps.ese.controller.simulation.loader.io.{File, Folder, NotExistingFile, NotExistingFolder}
 import org.apache.commons.io.FileUtils
 import org.kaikikm.threadresloader.ResourceLoader
 import org.scalatest.FunSuite
@@ -24,7 +25,7 @@ class TestYamlSaver extends FunSuite {
     DataValidator.validateNewSimulationData(data)
     val tmpFolder = Folder(Files.createTempDirectory("ese").toFile)
     val saver = YamlSaver(data, "test")
-    saver.saveData(tmpFolder, false)
+    saver.saveData(tmpFolder, overrideAll = false)
     tmpFolder.getChildren("test.yml") match {
       case f: File =>
         YamlLoader.loadCompleteSimulation(f) match {
@@ -36,7 +37,50 @@ class TestYamlSaver extends FunSuite {
       case _ =>
         fail()
     }
+    FileUtils.deleteDirectory(tmpFolder.rawFile)
+  }
 
+  test("An already existing resource causes expected failure. Resource can be added to override list") {
+    val data: CompleteSimulationData = YamlLoader.loadCompleteSimulation(File(ResourceLoader
+      .getResource("it/unibo/pps/ese/controller/simulation/loader/newsimulation/Simulation.yml"))) match {
+      case Success(value) =>
+        value
+      case Failure(_) =>
+        fail()
+    }
+    DataValidator.validateNewSimulationData(data)
+    val tmpFolder = Folder(Files.createTempDirectory("ese").toFile)
+    val saver = YamlSaver(data, "test")
+    tmpFolder.getChildren("test.yml") match {
+      case f: NotExistingFile =>
+        f.createFile()
+      case _ =>
+        fail()
+    }
+    saver.saveData(tmpFolder, overrideAll = false) match {
+      case Success(_) =>
+        fail()
+      case Failure(exception: ResourceAlreadyExistsException) =>
+        saver.addResourceToOverride(exception.existingResource)
+        saver.saveData(tmpFolder, overrideAll = false) match {
+          case Failure(_) =>
+            fail()
+          case _ =>
+        }
+      case _ =>
+        fail()
+    }
+    tmpFolder.getChildren("test.yml") match {
+      case f: File =>
+        YamlLoader.loadCompleteSimulation(f) match {
+          case Success(value) =>
+            DataValidator.validateNewSimulationData(value)
+          case _ =>
+            fail()
+        }
+      case _ =>
+        fail()
+    }
     FileUtils.deleteDirectory(tmpFolder.rawFile)
   }
 }
